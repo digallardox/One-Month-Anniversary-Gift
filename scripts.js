9cbdde601973bbed80b8952e77b91d45d15c5af9
digallardox <digallardox@gmail.com>
2021-12-09 15:33:41 -0600

    done

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let totalCallTime = 23 + 1 + 36 + 3 + 27 + 1 + 5 + 14 + 48 + 7 + 1 + 1 + 17 + 28 + 52 + 2 + 29 + 7 + 97 + 26 + 7 + 2 + 15 + 1 + 18 + 14 + 84 + 12 + 6 + 2 + 8 + 23 + 35 + 65 + 16 + 46 + 1 + 17 + 3 + 41 + 6 + 2 + 11 + 7 + 1 + 9 + 3 + 13 + 19 + 67 + 31 + 67 + 4 + 16 + 2 + 1 + 1 + 28 + 1 + 9 + 33 + 1 + 25 + 78;
+console.log(totalCallTime/60);
+
+let textLength = 'Love, Diego Gallardo';
+console.log(textLength.length);
+
+// const textOne = document.getElementById('textOne').innerText.length;
+// console.log(textOne);
+
+// const textTwo = document.getElementById('textTwo').innerText.length;
+// console.log(textTwo);
